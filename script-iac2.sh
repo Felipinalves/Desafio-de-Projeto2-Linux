30c934d01e7bb739a65a3d46705d59adeb20c1c9
Felipe Alves Cerquiare <71992399+Felipinalves@users.noreply.github.com>
2022-08-16 22:56:56 -0300

    Update script-iac2.sh

--- script-iac2.sh
+++ script-iac2.sh
@@ -1,7 +1,4 @@
-wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
-unzip main.zip
-cd linux-site-dio-main
-cp -R * /var/www/html/#!/bin/bash
+#!/bin/bash
 
 echo "Atualizando o servidor..."
 
